3ec2f9903a2f004575873f32d252797272d492a6
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 11:51:32 +0400

    Implement method for add new Vehicle

--- src/VehicleManager.java
+++ src/VehicleManager.java
@@ -10,5 +10,23 @@ public class VehicleManager {
         vehicles.add(new Vehicle("V103", "Hyundai Elantra", 22));
     }
 
+    public String addVehicle(Vehicle vehicle){
+        for (Vehicle v : vehicles) {
+            if (v.getVehicleId().equals(vehicle.getVehicleId())) {
+                return "Vehicle ID already exists \n" +
+                        "No vehicle was added";
+            }
+        }
+
+        vehicles.add(vehicle);
+
+        return "****************************\n" +
+                "Vehicle Added Successfully\n"
+                + "Vehicle ID: " + vehicle.getVehicleId() + "\n"
+                + "Vehicle Model: " + vehicle.getVehicleModel() + "\n"
+                + "Rental Price Per Day: " + vehicle.getRentalPricePerDay() + " OMR\n"
+                + "Status: Created\n"+
+                "***************************\n";
+    }
 
 }
