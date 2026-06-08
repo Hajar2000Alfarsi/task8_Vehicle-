ed7a99646ebe43e7c532414273ba2ed45b7d9950
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 11:39:17 +0400

    Implment constructor, getters, setters, and toString

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,4 +2,43 @@ public class Vehicle {
     private String vehicleId;
     private String vehicleModel;
     private double rentalPricePerDay;
+
+    public Vehicle(String vehicleId, String vehicleModel, double rentalPricePerDay) {
+        this.vehicleId = vehicleId;
+        this.vehicleModel = vehicleModel;
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public double getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+
+    public void setRentalPricePerDay(double rentalPricePerDay) {
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
+
+    @Override
+    public String toString() {
+        return "----------------\n" +
+                "Vehicle: " +   "\n" +
+                "vehicleId: " + vehicleId + '\n' +
+                "vehicleModel: " + vehicleModel + '\n' +
+                "rentalPricePerDay: " + rentalPricePerDay;
+    }
 }
